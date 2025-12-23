3068e0b616a2c83f1aa8c43333b2c7e28adbb28a
Kimberly Lin <dazeiihaze@gmail.com>
2025-12-23 09:56:22 -0700

    Update features.py

--- src/features.py
+++ src/features.py
@@ -1,15 +1,40 @@
+# src/features.py
 from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+import numpy as np
 
-def create_features(df):
+label_encoders = {}
+
+def create_features(df, training=True):
     df = df.copy()
-    df['year_month'] = df['prev_sold_date'].dt.to_period('M').astype(str)
-
-    for col in ['zip_code','season','metromicro']:
-        le = LabelEncoder()
-        df[col] = le.fit_transform(df[col].astype(str))
-
+    
+    # Build up year_month
+    if 'prev_sold_date' in df.columns:
+        df['year_month'] = df['prev_sold_date'].apply(
+            lambda x: x.to_period('M').strftime("%Y-%m") if pd.notnull(x) else 'unknown'
+        )
+    else:
+        df['year_month'] = 'unknown'
+    
+    categorical_cols = ['zip_code','season','metromicro','year_month']
     features = ['bed','bath','acre_lot','zip_code','season','metromicro']
-    target = 'price'
-    X = df[features]
-    y = df[target]
-    return X, y
+
+    if training:
+        # Fit LabelEncoder when training
+        for col in categorical_cols:
+            le = LabelEncoder()
+            df[col] = df[col].astype(str)
+            df[col] = le.fit_transform(df[col])
+            label_encoders[col] = le
+        # Perfrom log1p when getting target
+        X = df[features]
+        y = np.log1p(df['price'])
+        return X, y
+    else:
+        # Only transform when predicting
+        for col in categorical_cols:
+            le = label_encoders[col]
+            df[col] = df[col].astype(str).apply(lambda x: x if x in le.classes_ else 'unknown')
+            df[col] = le.transform(df[col])
+        X = df[features]
+        return X, None
